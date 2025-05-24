de0ed95f39a18112fe6c7cbbd58e8c91d9147b78
GDSofKY-dev <sandeshlamichhane@Sandeshs-MacBook-Pro.local>
2025-05-24 11:35:26 -0400

    updated favicon and title

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import { astroImageTools } from "astro-imagetools";
 export default defineConfig({
     site: "https://github.com/GDSofKY-dev.github.io",
     output: 'static',
-    base: 'GDSOFKY-DEV.GITHUB.IO',
+    // base: 'GDSOFKY-DEV.GITHUB.IO',
     integrations: [astroImageTools]
 });
