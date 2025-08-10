ebccbfcbc7756d1e49134e3344658f8d537fb350
Sandesh-l <sandeshlamichhane@Sandeshs-MacBook-Pro.local>
2025-08-10 16:37:01 -0400

    modified some code for better seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import { astroImageTools } from "astro-imagetools";
+import sitemapPlugin from "@astrojs/sitemap"
 // https://astro.build/config
 export default defineConfig({
     site: "https://github.com/GDSofKY-dev.github.io",
     output: 'static',
     // base: 'GDSOFKY-DEV.GITHUB.IO',
-    integrations: [astroImageTools]
+    integrations: [astroImageTools, sitemapPlugin()]
 });
