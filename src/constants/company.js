870beaad679542d91a3bcb34fc36c48f5a5dc3ef
Sandesh-l <Sandesh.lamichhane1@gmail.com>
2026-01-21 17:00:38 -0500

    changed yelp to nextdoor

--- src/constants/company.js
+++ src/constants/company.js
@@ -5,19 +5,19 @@ export const COMPANY_INFO = {
     email: "sales@garagedoorsolutionsofky.com",
     emailHref: "mailto:sales@garagedoorsolutionsofky.com",
     address: {
-        street: "536 Blue Spruce Dr.",
+        street: "",
         city: "Richmond",
         state: "KY",
         zip: "40475",
-        full: "123 Main Street, Richmond, KY 40475"
+        full: "Richmond, KY 40475"
     },
     hours: {
-        weekdays: "6:00 AM - 9:00 PM",
-        weekends: "6:00 AM - 9:00 PM"
+        weekdays: "6:00 AM - 6:00 PM",
+        weekends: "6:00 AM - 6:00 PM"
     },
     social: {
         facebook: "https://www.facebook.com/gdsofky",
         google: "#",
-        yelp: "#"
+        nextdoor: "https://nextdoor.com/page/garage-door-solutions-of-ky-richmond-ky"
     }
 }
