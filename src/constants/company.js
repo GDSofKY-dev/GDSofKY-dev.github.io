ae47beb10ac70fe2d3603303f07d5fdab8d3a062
Sandesh-l <Sandesh.lamichhane1@gmail.com>
2026-01-28 10:14:12 -0500

    updated address

--- src/constants/company.js
+++ src/constants/company.js
@@ -5,12 +5,13 @@ export const COMPANY_INFO = {
     email: "sales@garagedoorsolutionsofky.com",
     emailHref: "mailto:sales@garagedoorsolutionsofky.com",
     address: {
-        street: "",
+        street: "165 Charlie Norris Rd Unit B",
         city: "Richmond",
         state: "KY",
         zip: "40475",
         full: "Richmond, KY 40475"
     },
+    addressHref: "https://www.google.com/maps/place/165+Charlie+Norris+Rd,+Richmond,+KY+40475/@37.7424117,-84.2034205,19z/data=!3m1!4b1!4m6!3m5!1s0x884303e66a1b0e79:0x17ffeda783e29897!8m2!3d37.7424117!4d-84.2027768!16s%2Fg%2F11h9qf627g?entry=ttu&g_ep=EgoyMDI2MDEyNS4wIKXMDSoASAFQAw%3D%3D",
     hours: {
         weekdays: "6:00 AM - 6:00 PM",
         weekends: "6:00 AM - 6:00 PM"
